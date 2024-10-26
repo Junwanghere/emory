205dab34f681bb23f2f53cc680ea036d070786a3
Junwanghere <change60201@gmail.com>
2024-10-26 17:34:42 +0800

    post page add close function

--- src/views/Post/index.vue
+++ src/views/Post/index.vue
@@ -1,16 +1,34 @@
 <script setup>
-import { ref } from 'vue'
+import { onUnmounted, ref } from 'vue'
 import emitter from '@/utils/emitter';
 
 const show = ref(false)
 emitter.on('open-post',(value) => {
   show.value = value
 })
+
+onUnmounted(() => {
+  emitter.off('open-post')
+})
 </script>
 
 <template>
-    <van-popup v-model:show="show" position="bottom" :style="{ height: '100%' }">内容</van-popup>
+  <van-popup v-model:show="show" position="bottom" :style="{ height: '100%' }">内容
+    <div class="close-btn" @click="show = false">
+      <van-icon size="1.2rem" name="cross" color="#000000"/>
+    </div>
+  </van-popup>
 </template>
 
-<script>
-</script>
+<style scoped>
+.close-btn {
+  background-color: #e0e0e0;
+  width: 2rem;
+  height: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+}
+
+</style>
